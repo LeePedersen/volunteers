e64ee0bd90355f768eb4b5ef0704fb14c9010e25
Lee Pedersen <leepedersenr@gmail.com>
2020-01-10 09:37:11 -0800

    should pass first six or so tests now (no way of knowing because rspec is not working)

--- lib/project.rb
+++ lib/project.rb
@@ -6,4 +6,35 @@ class Project
     @title = attributes.fetch(:title)
   end
 
+  def save
+    result = DB.exec("INSERT INTO projects (name, id) VALUES ('#{name}', #{id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def ==(project)
+    self.name().downcase().eql?(project.name().downcase())
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each do |project|
+      id = project.fetch("id").to_i
+      title = project.fetch("title")
+      projects.push(Project.new({:id => id, :title => title}))
+    end
+  end
+
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def self.find(id)
+    project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+    id = project.fetch("id").to_i
+    title = project.fetch("title")
+    Project.new({:id => id, :title => title})
+  end
+
 end
