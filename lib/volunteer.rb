cec6a4113e1f6cfa67b059947d189ae97f2b1cac
Lee Pedersen <leepedersenr@gmail.com>
2020-01-10 09:49:59 -0800

    should pass couple of specs for volunteer

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,2 +1,10 @@
 class Volunteer
+  attr_accessor :name, :project_id, :id
+
+  def initialize attributes
+    @id = attributes.fetch :id
+    @name = attributes.fetch :name
+    @project_id = attributes.fetch :project_id
+  end
+
 end
